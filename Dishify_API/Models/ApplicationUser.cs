69c1eaca0ed2ccf078c6ddb628ef6bce985ec577
Elmedin Llumnica <elmedinllumnica@gmail.com>
2026-02-06 10:48:51 +0100

    fix: complete user model and project setup

---
+++ Dishify_API/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Dishify_API.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
--- Dishify_API/Program.cs
+++ Dishify_API/Program.cs
@@ -1,21 +1,40 @@
+
+using Dishify_API.Data;
+using Dishify_API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
+builder.Services.AddSingleton<ApiResponse>();
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 
+}
+app.UseStaticFiles();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
