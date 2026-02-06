bdf915d1ee714e5d9a6843c63e2ad8da7bcd7de2
Elmedin Llumnica <elmedinllumnica@gmail.com>
2026-02-06 10:47:07 +0100

    feat(api): add DTOs and response models

---
+++ Dishify_API/Models/ApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace Dishify_API.Models
+{
+    public class ApiResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool isSuccess { get; set; } = true;
+        public List<string> ErrorMessages { get; set; } = [];
+        public object? Result { get; set; }
+
+    }
+}
