6aa39ce504a67fb1126de5fdcccc83e6b60adb0a
Elmedin Llumnica <elmedinllumnica@gmail.com>
2026-02-18 08:55:29 +0100

    Add authentication controllers for user management

---
+++ Dishify_API/Controllers/AuthTestController.cs
@@ -0,0 +1,26 @@
+﻿using Dishify_API.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dishify_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthTestController : Controller
+    {
+        [HttpGet]
+        [Authorize]
+        public ActionResult<string> GetSomething()
+        {
+            return "You are authorized user";
+        }
+
+        [HttpGet("{someValue:int}")]
+        [Authorize]
+        public ActionResult<string> GetSomething(int someValue)
+        {
+            return "You are authorized admin user";
+
+        }
+    }
+}
