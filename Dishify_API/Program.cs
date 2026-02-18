3a24637a38d6ebd61b7e8812081774dd5866f4d0
Elmedin Llumnica <elmedinllumnica@gmail.com>
2026-02-18 08:56:43 +0100

    Update program startup configuration

--- Dishify_API/Program.cs
+++ Dishify_API/Program.cs
@@ -1,9 +1,15 @@
 
 using Dishify_API.Data;
 using Dishify_API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.OpenApi;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Tokens.Experimental;
+using Microsoft.OpenApi.Models;
 using Scalar.AspNetCore;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,8 +23,28 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllers();
+
+var key = builder.Configuration.GetValue<string>("ApiSettings:Secret");
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = false,
+
+    };
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>options.AddDocumentTransformer<BearerSecuritySchemeTransformer>());
 builder.Services.AddSingleton<ApiResponse>();
 
 
@@ -36,9 +62,46 @@ app.UseStaticFiles();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+
+internal sealed class BearerSecuritySchemeTransformer
+(Microsoft.AspNetCore.Authentication.IAuthenticationSchemeProvider schemeProvider)
+   : IOpenApiDocumentTransformer
+{
+    public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    {
+ var authenticationSchemes = await schemeProvider.GetAllSchemesAsync();
+ if (authenticationSchemes.Any
+(authScheme => authScheme.Name == JwtBearerDefaults.AuthenticationScheme))
+        {
+            var requirement = new Dictionary<string, OpenApiSecurityScheme>
+            {
+                [JwtBearerDefaults.AuthenticationScheme] = new OpenApiSecurityScheme
+                {
+
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    In = ParameterLocation.Header,
+                    BearerFormat = "JWT",
+
+                }
+            };
+                document.Components ??= new OpenApiComponents();
+                document.Components.SecuritySchemes =requirement;
+        }
+
+        document.Info = new()
+        {
+            Title = "Dishify API",
+            Version = "v1",
+            Description = "API for Dishify application",
+
+        };
+    }
+}
