7fec7620fe5d6d9d0291d468bd2d587b92002963
Elmedin Llumnica <elmedinllumnica@gmail.com>
2026-02-18 08:56:01 +0100

    Add Static Details utility class

---
+++ Dishify_API/Utility/SD.cs
@@ -0,0 +1,10 @@
+﻿namespace Dishify_API.Utility
+{
+    public static class SD
+    {
+
+        public const string Role_Admin = "Admin"; 
+        public const string Role_Customer = "Customer";
+
+    }
+}
