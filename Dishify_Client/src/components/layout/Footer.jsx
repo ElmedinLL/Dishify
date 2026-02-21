a750db8327c12637e58a0f1c3d1764bb90c88ebc
Elmedin Llumnica <elmedinllumnica@gmail.com>
2026-02-22 00:09:42 +0100

    add: header & footer component

---
+++ Dishify_Client/src/components/layout/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+       <footer className="mt-auto py-3 bg-body-tertiary border-top">
+      <div className="container small text-muted  flex-sm-row text-center ">
+        <span >
+        Made by
+         Elmedin
+        </span>
+      </div>
+    </footer>
+    </div>
+  )
+}
