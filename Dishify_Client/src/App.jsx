7d674e4ab731a94576e7da51256a39981e323dda
Elmedin Llumnica <elmedinllumnica@gmail.com>
2026-02-22 00:09:04 +0100

    init: create React project

---
+++ Dishify_Client/src/App.jsx
@@ -0,0 +1,18 @@
+import AppRoutes from "./router/AppRouter"
+import Header from "./components/layout/Header"
+import Footer from "./components/layout/Footer"
+function App() {
+
+
+  return (
+    <div className="d-flex flex-column min-vh-100 bg-body">
+     <Header/>
+     <main className="flex-grow-1">
+     <AppRoutes/>
+     </main>
+     <Footer/>
+    </div>
+  )
+}
+
+export default App
