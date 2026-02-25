faa9d0e30fca7ef7f864169d411689e64a5d5560
Elmedin Llumnica <elmedinllumnica@gmail.com>
2026-02-25 13:09:31 +0100

    feat: Integrate menu item API with Redux store

---
+++ Dishify_Client/src/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { baseApi } from "./api/baseApi";
+
+export const store = configureStore({
+    reducer : {
+        [baseApi.reducerPath] : baseApi.reducer 
+    }
+    ,
+    middleware : (getDefaultMiddleWare) => getDefaultMiddleWare().concat(baseApi.middleware)
+})
