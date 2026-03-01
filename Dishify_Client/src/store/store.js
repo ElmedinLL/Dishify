73e8a9d24fe966036c22bbfe27daf55b0f1ba2c0
Elmedin Llumnica <elmedinllumnica@gmail.com>
2026-03-01 13:17:49 +0100

    Add menu items state management

--- Dishify_Client/src/store/store.js
+++ Dishify_Client/src/store/store.js
@@ -1,10 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { baseApi } from "./api/baseApi";
+import { configureStore } from "@reduxjs/toolkit"
+import { baseApi } from "./api/baseApi"
+import cartReducer from "./slices/cartSlice"
+import authReducer from "./slices/authSlice"
+import checkoutReducer from "./slices/checkoutSlice"
+import themeReducer from "./slices/themeSlice"
 
 export const store = configureStore({
-    reducer : {
-        [baseApi.reducerPath] : baseApi.reducer 
-    }
-    ,
-    middleware : (getDefaultMiddleWare) => getDefaultMiddleWare().concat(baseApi.middleware)
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    cart: cartReducer,
+    auth: authReducer,
+    checkout: checkoutReducer,
+    theme: themeReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 })
